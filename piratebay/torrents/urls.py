9156934aea047ec6b9efed26455941e838698594
Keyur Potdar <keyurpotdar@gmail.com>
2018-07-06 22:43:55 +0530

    Update urls.py

--- piratebay/torrents/urls.py
+++ piratebay/torrents/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from . import views
 
 app_name = 'torrents'
-
 urlpatterns = [
     path('', views.HomePage.as_view(), name='home'),
     path('search/', views.search, name='search'),
